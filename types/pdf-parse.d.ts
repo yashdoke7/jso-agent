62d9d85572b142d46222c0a9582a49f9a0f9e845
yashdoke7 <yashdoke215@gmail.com>
2026-03-15 11:14:55 +0530

    fix: add type declaration for pdf-parse subpath import

--- types/pdf-parse.d.ts
+++ types/pdf-parse.d.ts
@@ -7,3 +7,13 @@ declare module "pdf-parse" {
   function pdfParse(buffer: Buffer): Promise<PDFData>;
   export default pdfParse;
 }
+
+declare module "pdf-parse/lib/pdf-parse.js" {
+  interface PDFData {
+    text: string;
+    numpages: number;
+    info: Record<string, unknown>;
+  }
+  function pdfParse(buffer: Buffer): Promise<PDFData>;
+  export default pdfParse;
+}
